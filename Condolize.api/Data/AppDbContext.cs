4932bfa0c71d2b713d01966d3cbaee2bdcc2fdde
Administrador <Administrador@5CG9216CBP>
2026-05-19 20:09:08 -0300

    Add project files.

---
+++ Condolize.api/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Condolize.api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Condolize.api.Data
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Association> Associations => Set<Association>();
+
+    }
+}
---
+++ Condolize.api/Entities/Association.cs
@@ -0,0 +1,8 @@
+﻿namespace Condolize.api.Entities
+{
+    public class Association
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
