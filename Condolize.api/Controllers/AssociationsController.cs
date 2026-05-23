f926931ec0acad2fb433cc7a2c29b2f89d8ee602
Administrador <Administrador@5CG9216CBP>
2026-05-23 14:32:07 -0300

    Conexão estabelecida com banco, primeira tabela criada

---
+++ Condolize.api/Controllers/AssociationsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Condolize.api.Data;
+using Condolize.api.Entities;
+using Condolize.api.DTOs;
+
+namespace Condolize.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AssociationsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public AssociationsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var associations = _context.Associations.ToList();
+            return Ok(associations);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateAssociationDto dto)
+        {
+            var association = new Association
+            {
+                Name = dto.Name
+            };
+            await _context.Associations.AddAsync(association);
+
+            await _context.SaveChangesAsync();
+
+            return Ok(association);
+        }
+        
+
+    }
+}
---
+++ Condolize.api/DTOs/CreateAssociationDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Condolize.api.DTOs
+{
+    public class CreateAssociationDto
+    {
+        public string Name { get; set; } = string.Empty;
+    }
+}
